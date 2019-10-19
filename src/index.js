f0de792faa12654da7a8123a6b956a24988f6643
Joe <joedravarol@gmail.com>
2019-10-20 00:10:25 +0100

    Setup navtab listener to render content

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import { setupInitialPage } from './modules/initial-page-load'
+import { createHomeContent } from './modules/home'
+import { createMenuContent } from './modules/menu'
+import { createContactContent } from './modules/contact'
+
+const setupNavListener = () => {
+  const mainContainer = document.querySelector('.main-container');
+  const navTab = document.querySelector('.primary-nav');
+
+  navTab.addEventListener('click', e => {
+    e.preventDefault();
+    
+    const elementText = e.target.textContent;
+    // Use to prevent generating the same content
+    const currentContentClassName = mainContainer.lastChild.className;
+
+    if (elementText) {
+      if (elementText === "Home" && currentContentClassName !== "home") {
+        mainContainer.removeChild(mainContainer.lastChild);
+        mainContainer.appendChild(createHomeContent());
+
+      } else if (elementText === "Menu" && currentContentClassName !== "menu") {
+        mainContainer.removeChild(mainContainer.lastChild);
+        mainContainer.appendChild(createMenuContent());
+
+      } else if (elementText === "Contact" && currentContentClassName !== "cafe-info") {
+        
+        mainContainer.removeChild(mainContainer.lastChild);
+        mainContainer.appendChild(createContactContent());
+      };
+    };
+  });
+};
 
 setupInitialPage();
+setupNavListener();
